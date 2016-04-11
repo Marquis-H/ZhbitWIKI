5399cde2896c4e5bd449efc1caf5286aa28189b9
marquis <511680713@qq.com>
2016-04-12 00:47:01 +0800

    add exception

---
+++ src/ZhbitWIKI/WechatBundle/Exception/UserNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marquis
+ * Date: 16/4/12
+ * Time: 上午12:40
+ */
+namespace ZhbitWIKI\WechatBundle\Exception;
+
+/**
+ * Class UserNotFoundException
+ * @package ZhbitWIKI\WechatBundle\Exception
+ */
+class UserNotFoundException extends \Exception
+{
+
+}
